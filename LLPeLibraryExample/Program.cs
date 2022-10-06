c28aeb8a2f7243cfec78babb44615d74f828cb67
crowbarmaster <crowbarmasterhl2@gmail.com>
2022-10-05 22:13:38 -0400

    Initial commit of .NET Core example usage of LLPeProviderService.dll

---
+++ LLPeLibraryExample/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Runtime.InteropServices;
+
+public static class PeLibraryTests {
+	enum LLFileTypes {
+		VanillaExe,
+		LiteModExe,
+		BedrockPdb,
+		ApiDef,
+		VarDef,
+		SymbolList
+	}
+
+	[DllImport("LLPeProviderService.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "?CreateModifiedExecutable@LLPE@@YA_NXZ")]
+    public static extern bool CreateModifiedExecutable();
+
+	[DllImport("LLPeProviderService.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "?SetEditorFilename@LLPE@@YAXHPEBD@Z")]
+	public extern static void SetEditorFilename(int fileTpe, char[] filenameCharArray);
+
+	[DllImport("LLPeProviderService.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "?ProcessPlugins@LLPE@@YA_NXZ")]
+	public extern static bool ProcessPlugins();
+
+	public static void RunTest() {
+		SetEditorFilename((int)LLFileTypes.LiteModExe, "BedrockService.Test.exe".ToCharArray());
+		ProcessPlugins();
+		CreateModifiedExecutable();
+    }
+
+	public static void Main(string[] args) {
+		RunTest();
+	}
+}
